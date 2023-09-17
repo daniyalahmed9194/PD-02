2dace7e7922f4a40bf70b0356ac987b576eb013f
Abdur Rehman Daniyal <144408207+daniyalahmed9194@users.noreply.github.com>
2023-09-17 16:11:47 +0500

    Add files via upload

---
+++ alphabetprint.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("Color 95");
+
+cout<<" ####"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"######"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"      "<<endl;      
+cout<<"##  ##"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"####"<<endl;
+cout<<"##"<<endl;
+cout<<"####"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"      "<<endl;
+cout<<"      "<<endl;
+cout<<"#######"<<endl;
+cout<<"     ##"<<endl;
+cout<<"    ##"<<endl;
+cout<<"  ##"<<endl;
+cout<<" ##"<<endl;
+cout<<"#######"<<endl;
+cout<<"      "<<endl;
+cout<<"##  ##"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"####"<<endl;
+cout<<"##"<<endl;
+cout<<"####"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"      "<<endl;
+cout<<"      "<<endl;
+cout<<" ####"<<endl;
+cout<<"##  ##"<<endl;
+cout<<"##"<<endl;
+cout<<" ####"<<endl;
+cout<<"    ##"<<endl;
+cout<<"##  ##"<<endl;
+cout<<" ####"<<endl;
+}
---
+++ cow.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"          O"<<endl;
+cout<<"                 ^__^"<<endl;
+cout<<"              O  (oo)\\___________"<<endl;
+cout<<"                 (__)\\           )\\/\\"<<endl;
+cout<<"                     | |--------W |"<<endl;
+cout<<"                     | |        | |"<<endl;
+}
